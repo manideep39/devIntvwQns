20299f80dbc1bac1a7caa88d0c765709d256dfd1
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2021-03-11 09:59:43 +0530

    modified yangshun script and added css markdown and scraped json data of yangshun css

--- scrapingScripts/yangshun.js
+++ scrapingScripts/yangshun.js
@@ -5,12 +5,15 @@ const cssFileScraper = new Scraper("../markdown/yangshun/css.md");
 const jsFileScraper = new Scraper("../markdown/yangshun/javascript.md");
 
 scrapeYangshun(htmlFileScraper, "./yangshunData/yangshunHtml.json");
+scrapeYangshun(cssFileScraper, "./yangshunData/yangshunCss.json");
 
 function scrapeYangshun(scraper, fileName) {
   // console.log(scraper.showMarkdown);
   // console.log(scraper.showHtml);
   let count = 0;
   const document = scraper.createDOM();
+  const tableOfCont = document.querySelectorAll("[href='#table-of-contents']");
+  [...tableOfCont].map((ele) => ele.parentNode.remove());
   const bodyChildren = [...document.body.children];
   const questions = [];
   let start = false;
