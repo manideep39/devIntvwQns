d171379d6e75de4a70e8ded45b51bbfb1b65a641
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2021-03-16 11:48:45 +0530

    handling the case of no exp or stat

--- scrapeTester.js
+++ scrapeTester.js
@@ -11,13 +11,19 @@ class ScrapeTester extends Scraper {
     const json = fs.readFileSync(this.jsonFileLoc, "utf-8");
     const { questions } = JSON.parse(json);
     const document = this.createDOM("<html><body></body></html>");
-    questions.forEach((node) => {
-      const statement = document.createElement("h3");
-      statement.innerHTML = this.markdown2Html(node.statement);
-      const explanation = document.createElement("div");
-      explanation.innerHTML = this.markdown2Html(node.explanation);
-      const hr = document.createElement("hr");
-      document.body.append(statement, explanation, hr);
+    questions.forEach((node, ind) => {
+      if (node.statement && node.explanation) {
+        const statement = document.createElement("h3");
+        statement.innerHTML = this.markdown2Html(node.statement);
+        const explanation = document.createElement("div");
+        explanation.innerHTML = this.markdown2Html(node.explanation);
+        const hr = document.createElement("hr");
+        document.body.append(statement, explanation, hr);
+      } else {
+        console.log(
+          `Question ${ind + 1} has missing explanation or statement.`
+        );
+      }
     });
     this.saveHtml(saveHtmlTo, document.body.innerHTML);
   }
